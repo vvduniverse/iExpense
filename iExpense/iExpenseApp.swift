539b6feb2cb7f85d81ba737b840dab1326496ce9
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-04 06:35:25 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Vahtee Boo on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
